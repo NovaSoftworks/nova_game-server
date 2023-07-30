5290f0e867f5b2c9eb87fa69484c806472495c7a
marclamy <marc.lamy83@gmail.com>
2023-07-30 20:55:39 +0000

    add intro screen at server startup

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,11 @@
-import { NovaEngine } from './engine/nova-engine'
+console.log(`Welcome to`)
+console.log(` _   _  ______      __     
+| \\ | |/ __ \\ \\    / /      
+|  \\| | |  | \\ \\  / /  \\   
+| . \` | |  | |\\ \\/ / _\\ \\  
+| |\\  | |__| | \\  / ____ \\     
+|_| \\_|\\____/   \\/ /    \\_\\  - Dedicated Game Server               
+`)
 
-console.log(new NovaEngine().toString())
+console.log('Using Node.js version 20 (Bullseye)')
+
