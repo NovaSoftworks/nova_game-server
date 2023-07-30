0fb9748db998c0e88f3c2de7873234d7764a3f37
marclamy <marc.lamy83@gmail.com>
2023-07-30 22:13:54 +0000

    add basic websocket server using ws

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { NovaEngine } from "./engine/nova-engine"
+import { WebSocket } from 'ws'
 
 console.log(`Welcome to`)
 console.log(` _   _  ______      __     
@@ -12,3 +13,16 @@ console.log(` _   _  ______      __
 console.log('Using Node.js version 20 (Bullseye)')
 
 NovaEngine.start()
+
+const wss = new WebSocket.Server({ port: 80 })
+
+wss.on('connection', (ws) => {
+    console.log('New client connected')
+
+    ws.on('message', (message) => {
+        console.log(`Received message: ${message}`)
+        if (message.toString() == 'Hello, there!') {
+            ws.send('General Kenobi...')
+        }
+    })
+})
